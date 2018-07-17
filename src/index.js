65ed685519480a2a00f1678b93c8a9ca90c8a69a
Vamshi Boda <vaboda@microsoft.com>
2018-07-17 12:42:26 -0700

    Redux based stop watch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-let model = {
-  isRunning: false,
-  time: 0
-};
-
-const update = (model, intent) => {
+const update = (model={
+    isRunning: false,
+    time: 0
+  }, action) => {
   const updates = {
     TICK: model => {
       return Object.assign(
@@ -27,35 +26,38 @@ const update = (model, intent) => {
       return Object.assign(model, { isRunning: false });
     }
   };
-  return updates[intent](model);
+  return (updates[action.type] || (() => model))(model);
 };
+
+let stateContainer = createStore(update)
 
 let view = m => {
   let clickHandler = event => {
-    model = update(m, m.isRunning ? "STOP" : "START");
+    stateContainer.dispatch(m.isRunning ? { type: "STOP" } : { type: "START" });
   };
 
   return (
     <div>
-      {model.time}
+      {m.time}
       <br />
       <br />
       <br />
       <button onClick={clickHandler}>
         {" "}
-        {model.isRunning ? "Stop" : "Start"}
+        {m.isRunning ? "Stop" : "Start"}
       </button>
     </div>
   );
 };
 
 const render = () => {
-  ReactDOM.render(view(model), document.getElementById("root"));
+  ReactDOM.render(view(stateContainer.getState()), document.getElementById("root"));
 };
+
 render();
 registerServiceWorker();
+stateContainer.subscribe(render);
 
 setInterval(function() {
-  model = update(model, "TICK");
-  render();
+  stateContainer.dispatch({type: 'TICK'})
 }, 1000);
